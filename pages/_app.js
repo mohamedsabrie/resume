11763e6c547b235e722a597f9f5e69898613e6e8
mohamedsabrie <abosaadmohamed@gmail.com>
2022-02-15 00:57:43 +0200

    add some changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Mohamed Sabry Resume | Front End Developer</title>
+        <meta name="keywords" content="Front, React, Next" />
+        <meta
+          name="description"
+          content=" I am a Front End developer with industry experience building websites and web apps. I specialize in JavaScript and have professional experience working with React.js,
+          Next.js, and Tailwind.css. I also have experience working with Redux, TypeScript, and Firebase"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
