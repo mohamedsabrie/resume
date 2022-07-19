a18a13872ce544fe030def4b488efb8ecb17ed48
mohamedsabrie <abosaadmohamed@gmail.com>
2022-07-19 05:52:18 +0200

    stop the animation of the header and hero section at the beginning

--- pages/index.js
+++ pages/index.js
@@ -14,56 +14,56 @@ export default function Home() {
   
   // Initialise
   
-  useLayoutEffect(() => {
+  // useLayoutEffect(() => {
 
     
-  const t1 = gsap.timeline();
-  var mediaQuery = window.matchMedia("(min-width: 640px)");
+  // const t1 = gsap.timeline();
+  // var mediaQuery = window.matchMedia("(min-width: 640px)");
  
 
     
-    t1.from(".header_logo", {
-      opacity: 0,
-      x: -100,
-      ease: "power4.out",
-    },"+=1");
+  //   t1.from(".header_logo", {
+  //     opacity: 0,
+  //     x: -100,
+  //     ease: "power4.out",
+  //   },"+=1");
 
-    function buildConditionalTween(mediaQuery){
-      if(mediaQuery.matches){
-        gsap.utils.toArray("header li").forEach((link) => {
-          t1.from(link, {
-            opacity: 0,
-            x: 100,
-            ease: "power4.out", 
-          });
-        });
-      }else{
-         t1.from(".menu_icon", {
-          opacity: 0,
-          x: 100,
-          ease: "power4.out", 
-         })
-      }
-    };
-    buildConditionalTween(mediaQuery);
+  //   function buildConditionalTween(mediaQuery){
+  //     if(mediaQuery.matches){
+  //       gsap.utils.toArray("header li").forEach((link) => {
+  //         t1.from(link, {
+  //           opacity: 0,
+  //           x: 100,
+  //           ease: "power4.out", 
+  //         });
+  //       });
+  //     }else{
+  //        t1.from(".menu_icon", {
+  //         opacity: 0,
+  //         x: 100,
+  //         ease: "power4.out", 
+  //        })
+  //     }
+  //   };
+  //   buildConditionalTween(mediaQuery);
     
-    t1.from(".name", {
-      x: 100,
-      opacity: 0,
-      ease: "elastic",
-    })
-    .from(".job_title", {
-      x: -100,
-      opacity: 0, 
-      ease: "power4.out",
-    })
-    .from(".hero_buttons", {
-      y: 100,
-      opacity: 0, 
-      ease: "power3.out",
-    })
+  //   t1.from(".name", {
+  //     x: 100,
+  //     opacity: 0,
+  //     ease: "elastic",
+  //   })
+  //   .from(".job_title", {
+  //     x: -100,
+  //     opacity: 0, 
+  //     ease: "power4.out",
+  //   })
+  //   .from(".hero_buttons", {
+  //     y: 100,
+  //     opacity: 0, 
+  //     ease: "power3.out",
+  //   })
 
-  }, [])
+  // }, [])
 
   return (
     <div>
