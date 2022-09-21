ac66cc48a026b7894db09a08b39e30bebd24599e
mohamedsabrie <abosaadmohamed@gmail.com>
2022-09-21 18:47:56 +0200

    change the styling of the resume

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,6 +1,7 @@
 import { MailIcon } from "@heroicons/react/solid";
 import { Link } from "react-scroll";
 import { SocialIcon } from "react-social-icons";
+import Image from "next/image";
 
 function HeroSection() {
   const socials = [
@@ -21,9 +22,9 @@ function HeroSection() {
   return (
     <div id="about" className="bg-$yellow  bg-center  relative ">
       <div
-        className={`px-5 md:px-10 xl:px-20  relative flex flex-col md:flex-row justify-center md:justify-between gap-5  items-center betwee h-screen`}
+        className={`px-5 md:px-10 mt-10 xl:px-20  relative flex flex-col md:flex-row justify-center md:justify-between gap-5  items-center betwee h-screen`}
       >
-        <div className="md:mt-20 order-2 md:order-1 ">
+        <div className=" order-2 md:order-1 ">
           <h3 className=" text-center md:text-left ">
             <span className="name inline-block mb-3 font-extrabold text-$black1 text-2xl lg:text-3xl">
               {" "}
@@ -40,12 +41,11 @@ function HeroSection() {
             </span>
           </h3>
         </div>
-        <div className="mt-20 order-1 md:order-2 flex flex-col  md:flex-row items-center gap-10 md:gap-20">
-          <img
-            className="h-[275px] md:h-[300px] xl:h-[500px] transition duration-200 hover:rotate-2 "
-            src="/svgs/hero_img.svg"
-            alt=""
-          />
+        <div className="order-1 md:order-2 flex flex-col  md:flex-row items-center gap-10 md:gap-20">
+          <div className="relative w-[200px] h-[300px] md:w-[300px] md:h-[500px] lg:w-[500px] lg:h-[500px]  transition duration-200 hover:rotate-2 ">
+            <Image className="w-full h-full" src="/svgs/hero_img.svg" alt="" layout="fill" />
+          </div>
+
           <div className="flex space-x-5 md:flex-col md:space-y-5 md:space-x-0 justify-between items-center pb-5">
             {socials.map(({ href, name, id }) => (
               <div key={id} className=" ">
