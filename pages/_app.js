2414a14a0f4de2360a13f402d95817606a56e91e
mohamedsabrie <abosaadmohamed@gmail.com>
2022-10-04 13:52:53 +0200

    change the picture which appear for seo

--- pages/_app.js
+++ pages/_app.js
@@ -8,15 +8,15 @@ function MyApp({ Component, pageProps }) {
         title="Mohamed Sabry"
         titleTemplate="Mohamed Sabry"
         defaultTitle="Mohamed Sabry"
-        description="A front end web developer, who loves to develop beautiful websites and web apps . I have been coding for over a year now. I am professional using react.js-next.js "
+        description="A front end web developer, who loves to develop beautiful websites and web apps . I have been coding for about 2 years now. I am professional using react.js-next.js "
         canonical="https://mohamed-sabry-portfolio.vercel.app/"
         openGraph={{
           url: "https://mohamed-sabry-portfolio.vercel.app/",
           title: "Mohamed Sabry",
-          description: "A front end web developer, who loves to develop beautiful websites and web apps . I have been coding for over a year now. I am professional using react.js-next.js ",
+          description: "A front end web developer, who loves to develop beautiful websites and web apps . I have been coding for about 2 years now. I am professional using react.js-next.js ",
           images: [
             {
-              url: "/svgs/hero_img.svg",
+              url: "/og-image.svg",
               width: 800,
               height: 420,
               alt: "Mohamed Sabry",
