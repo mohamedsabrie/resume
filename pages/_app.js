fb1c6fdf4a410bac02b33d93d6f970037ca3d648
Mohamed Sabry <abosaadmohamed@gmail.com>
2024-07-24 22:21:49 +0700

    chaange the cv

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
         title="Mohamed Sabry"
         titleTemplate="Mohamed Sabry"
         defaultTitle="Mohamed Sabry"
-        description="A front end web developer, who loves to develop beautiful websites and web apps . I have been coding for about 2 years now. I am professional using react.js-next.js "
+        description="A front end web developer, who loves to develop beautiful websites and web apps . I have been coding for about +3 years now. I am professional using react.js-next.js "
         canonical="https://mohamed-sabry-portfolio.vercel.app/"
         openGraph={{
           url: "https://mohamed-sabry-portfolio.vercel.app/",
