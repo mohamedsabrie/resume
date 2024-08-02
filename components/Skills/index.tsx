23bee4d25676939e7f065f682a0b1238aa54eb81
Mohamed Sabry <abosaadmohamed@gmail.com>
2024-08-02 16:37:23 +0700

    add skills in skills page

--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -1,26 +1,54 @@
 import React from "react";
 import { Icon } from "@iconify-icon/react";
+import Image from "next/image";
 
 const skills = [
-  "html",
-  "css",
-  "javascript",
-  "typescript",
-  "react-dark",
-  "nextjs-dark",
-  "remix-dark",
-  "jquery",
-  "postman",
-  "redux",
-  "styledcomponents",
-  "git",
-  "github-dark",
-  "bitbucket-dark",
-  "materialui-dark",
-  "tailwindcss-dark",
-  "sass",
-  "less-dark",
-  "jest"
+  { name: "HTML", key: "html" },
+  { name: "CSS", key: "css" },
+  { name: "JavaScript", key: "javascript" },
+  { name: "TypeScript", key: "typescript" },
+  { name: "React JS", key: "react-dark" },
+  { name: "Next JS", key: "nextjs-dark" },
+  { name: "Remix", key: "remix-dark" },
+  { name: "jQuery", key: "jquery" },
+  { name: "Postman", key: "postman" },
+  { name: "Redux", key: "redux" },
+  { name: "Styled Components", key: "styledcomponents" },
+  { name: "Git", key: "git" },
+  { name: "GitHub", key: "github-dark" },
+  { name: "Bitbucket", key: "bitbucket-dark" },
+  { name: "Material UI", key: "materialui-dark" },
+  { name: "Tailwind CSS", key: "tailwindcss-dark" },
+  { name: "Sass", key: "sass" },
+  { name: "Less", key: "less-dark" },
+  { name: "Jest", key: "jest" },
+];
+
+const otherSkills = [
+  {
+    name: "React Query",
+    path: "/skills/react-query.svg",
+  },
+  {
+    name: "Ant Design",
+    path: "/skills/ant-design.svg",
+  },
+  {
+    name: "Recoil",
+    path: "/skills/recoil-js.svg",
+  },
+  {
+    name: "Firebase",
+    path: "/skills/firebase.svg",
+  },
+  {
+    name: "GraphQL",
+    path: "/skills/graphql.svg",
+  },
+  {
+    name: "Google Maps",
+    path: "/skills/google-maps.svg",
+  },
 ];
 function Skills() {
   return (
@@ -30,12 +58,23 @@ function Skills() {
     >
       <div className="max-w-5xl mx-auto flex flex-wrap items-center justify-center  gap-5 sm:gap-10">
         {skills.map((skill) => (
-          <div key={skill} className="flex flex-col items-center text-white font-semibold gap-y-1">
-            <Icon  height={50}  icon={`skill-icons:${skill}`} />
-            <h3>{skill.replace("-dark", "")}</h3>
+          <div
+            key={skill.name}
+            className="flex flex-col items-center text-white font-semibold gap-y-1"
+          >
+            <Icon height={50} icon={`skill-icons:${skill.key}`} />
+            <h3>{skill.name}</h3>
           </div>
         ))}
-        
+        {otherSkills.map((skill) => (
+          <div
+            key={skill.name}
+            className="flex flex-col items-center text-white font-semibold gap-y-1"
+          >
+            <Image height={50} width={50} src={skill.path} alt={skill.name} />
+            <h3>{skill.name}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
